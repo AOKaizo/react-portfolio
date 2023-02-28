dbb35e8f709a3c86c6b52221f910dde337cac907
AOKaizoMac <kolbywolf@gmail.com>
2023-02-27 22:37:45 -0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import "./App.css";
 
 function App() {
-  return <Box>
-    
-  </Box>;
+  return (
+    <Box>
+      <Typography>Kolby Wolf</Typography>
+    </Box>
+  );
 }
 
 export default App;
